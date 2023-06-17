b43e4a67b971b8df16b15cd97f3869e6ad58de5c
ashu <ashutosh.pednekar@affinsys.com>
2023-06-17 12:42:17 +0530

    feat: added reserve apis

--- store/api/views.py
+++ store/api/views.py
@@ -11,6 +11,15 @@ class FoodViewSet(viewsets.GenericViewSet):
 
     @action(methods=['POST'], detail=False)
     def reserve(self, request):
+        """
+        The reserve function is used to reserve a package of food.
+            The function takes in the name of the food and returns a message that says
+            &quot;Package: {food_name}({package_id}) has been reserved&quot; if successful.
+
+        :param self: Represent the instance of a class
+        :param request: Get the data from the request
+        :return: A message that the package has been reserved
+        """
         with transaction.atomic():
             food = Food.objects.get(name__icontains=request.data.get('name'))
             package = Package.objects.filter(is_reserved=False, food=food)
